54dd317c22a9d355ea2be603bf29072baadf70e3
adelliott92 <andrewdavidelliott@outlook.com>
2021-11-21 14:19:51 +0000

    :wrench: chore: updated tables in catalog app
    - Updated tables in models.py file for catalog app

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import display
+from .models import storeCategory, storeProduct
 
-# Register your models here.
+@admin.register(storeCategory)
+class storeAdmin(admin.ModelAdmin):
+    display_list = ['name', 'slug']
+    fields_prepopulated = {'slug': ('categoryName', )}
+
+@admin.register(storeProduct)
+class productAdmin(admin.ModelAdmin):
+    display_list = ['product_title', 'creator_id', 'slug', 'product_price', 'product_in_stock', 'created_on', 'updated_on']
+    filter_list = ['product_in_stock', 'product_is_active']
+    editable_list = ['product_price', 'product_in_stock']
+    fields_prepopulated = {'slug': ('product_title', )}
--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 class storeCategory(models.Model):
-    categoryName = models.CharField(max_length=255, db_index=True)
+    category_Name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
 
     class Meta:
@@ -21,17 +21,17 @@ class storeProduct(models.Model):
     product_image = models.ImageField(upload_to='images/')
     slug = models.SlugField(max_length=255)
     product_price = models.DecimalField(max_digits=4, decimal_places=2)
-    product_instock = models.BooleanField(default=True)
-    product_isactive = models.BooleanField(default=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+    product_in_stock = models.BooleanField(default=True)
+    product_is_active = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'Products'
-        ordering = ('-date_created',)
+        ordering = ('-created_on',)
     
     def get_absolute_url(self):
         return reverse('catalog:product_detail', args=[self.slug])
 
     def __str__(self):
-        return self.title
+        return self.name
