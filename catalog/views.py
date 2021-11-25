675ccfc885c9fcd06a6c6935163d21d01b729503
adelliott92 <andrewdavidelliott@outlook.com>
2021-11-25 20:49:52 +0000

    :sparkles: feat: add navigation bar to site
    - Create main css stylesheet for base.html
    - Create navigation links to navigation bar

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import storeCategory, storeProduct
 
+def categories(request):
+    return {
+        'categories': storeCategory.objects.all()
+    }
+
 def catalog_products(request):
     products = storeProduct.objects.all()
     return render(request, 'catalog/homepage.html', {'products': products})
--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
     'catalog',
 ]
 
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'catalog.views.categories',
             ],
         },
     },
@@ -120,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
