72803eb41800032be92b99d0b9eadbd743ef8478
adelliott92 <andrewdavidelliott@outlook.com>
2021-11-26 09:23:36 +0000

    :bug: fix: resolve issues with heroku deployment
    - Move static folder back to root
    - Move hero-image.jpg to static directory to resolve this issue "django.core.exceptions.SuspiciousFileOperation: The joined path (/tmp/build_68695899/assets/images/homepage/hero-image.jpg) is located outside of the base path component (/tmp/build_68695899/staticfiles)"w

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
     'catalog',
 ]
 
@@ -122,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
